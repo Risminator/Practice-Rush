fbf165a8ba2e6498bb843c8b0742e313cb977582
Risminator <vjdronov@yandex.ru>
2024-04-05 17:49:59 +0300

    feat: main setup

---
+++ global.gd
@@ -0,0 +1,6 @@
+extends Node
+
+const SCENES = {
+	MAIN = "res://scenes/main.tscn",
+	TEST_LEVEL = "res://scenes/levels/temp_test_level.tscn"
+}
---
+++ scenes/main.gd
@@ -0,0 +1,21 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+# Запуск игры
+func _on_btn_start_pressed():
+	get_tree().change_scene_to_file(Global.SCENES.TEST_LEVEL)
+
+
+# Выход из игры
+func _on_btn_exit_pressed():
+	get_tree().quit()
